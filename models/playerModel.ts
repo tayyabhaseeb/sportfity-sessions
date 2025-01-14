61a4bc1f8869c78675fb8a8c74763957afbebb58
Arian-Y <arianyousefi04@gmail.com>
2025-01-14 14:38:00 +0000

    pushing up some code

---
+++ models/playerModel.ts
@@ -0,0 +1,9 @@
+const db = require("../src/db/connection");
+
+function fetchPlayers() {
+  return db.query(`SELECT * FROM players`).then(({ rows }) => {
+    return rows;
+  });
+}
+
+module.exports = { fetchPlayers };
