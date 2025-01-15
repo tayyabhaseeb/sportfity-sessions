5e6e6e09a756240927ce81cf4b8b48b252b9539e
Arian-Y <arianyousefi04@gmail.com>
2025-01-15 16:28:21 +0000

    done teams

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 const { getOrganiser } = require("./controllers/organisersController");
 const { getPlayers } = require("./controllers/playersController");
+const { getTeams } = require("./controllers/teamsController");
 
 app.get("/api/players", getPlayers);
 
 app.get("/api/organisers", getOrganiser);
+
+app.get("/api/teams", getTeams);
 
 module.exports = { app };
