1be231e42d1730490d4ebc0b265389f9b928c55f
Ibrahim Shariff <imys@Ibrahims-MBP.lan>
2025-01-15 16:26:44 +0000

    leagues

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 const { getOrganiser } = require("./controllers/organisersController");
 const { getPlayers } = require("./controllers/playersController");
+const { getLeagues } = require("./controllers/leaguesController");
 
 app.get("/api/players", getPlayers);
 
 app.get("/api/organisers", getOrganiser);
+
+app.get("/api/leagues", getLeagues);
 
 module.exports = { app };
