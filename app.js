6916784b942dba55d65b9f09f8ec5e5bf5dd6a28
Ghiffari <ghiffariaulia90@gmail.com>
2025-01-16 13:38:38 +0000

    got rid of the duplicate of getMatches on app

--- app.js
+++ app.js
@@ -13,21 +13,16 @@ const {
 } = require("./controllers/managersController");
 const { getOrganiser } = require("./controllers/organisersController");
 
-
 app.use(express.json());
 
 //players
 app.get("/api/players", getPlayers);
 
-
 const { getMatches, getMatchById } = require("./controllers/matchesController");
 
-const { getMatches } = require("./controllers/matchesController");
 const { getTeamsById } = require("./controllers/teamsController");
 
-
 app.use(express.json());
-
 
 app.get("/api/players", getPlayers);
 
@@ -47,7 +42,6 @@ app.get("/api/leagues", getLeagues);
 //organisers
 app.get("/api/organisers", getOrganiser);
 
-
 app.use((err, req, res, next) => {
   if (err.status) {
     res.status(err.status).send({ msg: err.msg });
@@ -65,10 +59,8 @@ app.use((err, req, res, next) => {
 });
 app.get("/api/matches", getMatches);
 
-
 app.get("/api/matches/:match_id", getMatchById);
 
 app.get("/api/teams/:team_id", getTeamsById);
-
 
 module.exports = { app };
---
+++ src/db/data/test-data/queries.sql
@@ -0,0 +1,40 @@
+\c sports_app
+
+-- /api/components/match/:match_id
+
+SELECT matches.match_id, match_date, start_time, duration, team_name, league_name, score
+FROM matches
+INNER JOIN leagues
+ON matches.league_id = leagues.league_id
+INNER JOIN match_teams
+ON matches.match_id = match_teams.match_id
+INNER JOIN teams
+ON match_teams.team_id = teams.team_id
+WHERE matches.match_id = 2;
+
+-- /api/match/goals/:match_id
+
+SELECT matches.match_id, player_name, goals, team_name
+FROM matches
+INNER JOIN match_players
+ON matches.match_id = match_players.match_id
+INNER JOIN players
+ON match_players.player_id = players.player_id
+INNER JOIN team_players
+ON players.player_id = team_players.player_id
+INNER JOIN teams
+ON team_players.team_id = teams.team_id
+WHERE matches.match_id = 2;
+
+
+SELECT matches.match_id, team_name, player_name
+FROM matches
+INNER JOIN match_teams
+ON matches.match_id = match_teams.match_id
+INNER JOIN teams
+ON match_teams.team_id = teams.team_id
+INNER JOIN team_players
+ON teams.team_id = team_players.team_id
+INNER JOIN players
+ON team_players.player_id = players.player_id
+WHERE matches.match_id = 2;
