5dec557cd17386154a56294728cc37f5e9dc3ad9
“Tayyab <tayyabhaseeb5@gmail.com>
2025-01-16 11:37:18 +0000

    adds specific player end point

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
-const { getPlayers } = require("./controllers/playersController");
+const {
+  getPlayers,
+  getSpecificPlayer,
+} = require("./controllers/playersController");
 const { getLeagues } = require("./controllers/leaguesController");
 const { getTeams } = require("./controllers/teamsController");
 const { getAllManagers } = require("./controllers/managersController");
 const { getOrganiser } = require("./controllers/organisersController");
 
 app.get("/api/players", getPlayers);
+app.get("/api/players/:player_id", getSpecificPlayer);
 
 app.get("/api/managers", getAllManagers);
 
