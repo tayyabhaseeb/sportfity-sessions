1e7ff721a4a0c4e61dfe6d5059c946bb813b4b86
Bruno Jauad <brunojauad26@icloud.com>
2025-01-16 12:14:13 +0000

    created post managers journey

--- app.js
+++ app.js
@@ -3,17 +3,44 @@ const app = express();
 const { getPlayers } = require("./controllers/playersController");
 const { getLeagues } = require("./controllers/leaguesController");
 const { getTeams } = require("./controllers/teamsController");
-const { getAllManagers } = require("./controllers/managersController");
+const {
+  getAllManagers,
+  addManager,
+} = require("./controllers/managersController");
 const { getOrganiser } = require("./controllers/organisersController");
 
+app.use(express.json());
+
+//players
 app.get("/api/players", getPlayers);
 
+//managers
 app.get("/api/managers", getAllManagers);
+app.post("/api/managers", addManager);
 
+//teams
 app.get("/api/teams", getTeams);
 
+//leagues
 app.get("/api/leagues", getLeagues);
 
+//organisers
 app.get("/api/organisers", getOrganiser);
+
+app.use((err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).send({ msg: err.msg });
+  } else next(err);
+});
+
+app.use((err, req, res, next) => {
+  if (err.code === "22P02" || err.code === "23502") {
+    res.status(400).send({ msg: "Bad Request" });
+  } else next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ msg: "Internal Server Error" });
+});
 
 module.exports = { app };
